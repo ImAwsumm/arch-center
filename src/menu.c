168c7f15faeaffda3a0d35d765a7a3381375e887
ImAwsum <153707128+ImAwsumm@users.noreply.github.com>
2026-03-04 16:15:54 -0500

    fix int highlight being out of scope

--- src/menu.c
+++ src/menu.c
@@ -2,11 +2,10 @@
 
 int main() 
 {
-
     const char *choices[] = 
     {
+        "Option 0",
         "Option 1",
-        "Option 2",
         "Settings",
         "Exit",
     };
@@ -16,11 +15,12 @@ int main()
     // initialize ncurses
     init_menu();
 
+    choice = -1;
+    highlight = 0;
     while (1) 
     {
-	choice = -1;
-	highlight = 0;
         clear();
+        mvprintw(0, 2, "Main menu");
         mvprintw(1, 2, "Use arrow keys to move and Q to quit");
 
         for (int i = 0; i < n_choices; i++) 
@@ -59,75 +59,124 @@ int main()
                 break;
 
             case 'q':
-                endwin();
 		printw("Hey");
+		delay_time(0, 2);
+                endwin();
                 return 0;
+	    default:
+		choice = -1;
+		break;
         }
+
 	if (choice == 3)
 	{
 	    endwin();
 	    return 0;
 	}
+	else if (choice == 0)
+	{
+	    //settings_menu();
+	    // (aka nothing)
+	}
+	else if (choice == 1)
+	{
+	    //settings_menu();
+	    // (aka nothing)
+	}
 	else if (choice == 2)
 	{
 	    settings_menu();
+	    choice = -1;
+	    highlight = 0;
 	}
-
+	else
+	{
+	    choice = -1;
+	}
     }
     endwin();
     return 0;
 }
 
-void settings_menu()
+int settings_menu()
 {
-    const char *settings_menu_choices[] = 
+    highlight = 0;
+    while(1)
     {
-        "Configure this program",
-        "Packages",
-        "configure dotfiles",
-        "Exit",
-        "Exit",
-    };
-    clear();
-    mvprintw(1, 2, "Settings");
+	const char *settings_menu_choices[] = 
+    	{
+    	    "Configure this program",
+    	    "Packages",
+    	    "configure dotfiles",
+    	    "Back",
+    	};
 
-    int n_choices = ARRAY_SIZE(settings_menu_choices);
-    for (int i = 0; i < n_choices; i++) 
-    {
-        if (i == highlight)
-        {
-            attron(A_REVERSE);
-            mvprintw(3 + i, 4, "%s", settings_menu_choices[i]);
-            attroff(A_REVERSE);
-        }
-        else 
-        {
-            mvprintw(3 + i, 4, "%s", settings_menu_choices[i]);
-        }
+    	clear();
+
+    	mvprintw(0, 2, "Settings");
+    	mvprintw(1, 2, "Press \"b\" to go back");
+
+    	int n_choices = ARRAY_SIZE(settings_menu_choices);
+    	for (int i = 0; i < n_choices; i++) 
+    	{
+    	    if (i == highlight)
+    	    {
+    	        attron(A_REVERSE);
+    	        mvprintw(3 + i, 4, "%s", settings_menu_choices[i]);
+    	        attroff(A_REVERSE);
+    	    }
+    	    else 
+    	    {
+    	        mvprintw(3 + i, 4, "%s", settings_menu_choices[i]);
+    	    }
+    	}
+    	c = getch();
+    	switch (c) 
+    	{
+    	    case KEY_UP:
+    	        highlight--;
+    	        if (highlight < 0)
+    	            highlight = n_choices - 1;
+    	        break;
+
+    	    case KEY_DOWN:
+    	        highlight++;
+    	        if (highlight >= n_choices)
+    	            highlight = 0;
+    	        break;
+
+
+    	    case 10:  // Enter key
+    	        choice = highlight;
+    	        break;
+
+    	    case 'b':
+    	        endwin();
+		return 0;
+    	}
+
+	if (choice == 3)
+	{
+	    endwin();
+	    return 0;
+	}
+	else if (choice == 0)
+	{
+	    //settings_menu();
+	}
+	else if (choice == 1)
+	{
+	    //settings_menu();
+	}
+	else if (choice == 2)
+	{
+	    //settings_menu();
+	}
+	else
+	{
+	    choice = -1;
+	}
     }
-    c = getch();
-    switch (c) 
-    {
-        case KEY_UP:
-            highlight--;
-            if (highlight < 0)
-                highlight = n_choices - 1;
-            break;
-
-        case KEY_DOWN:
-            highlight++;
-            if (highlight >= n_choices)
-                highlight = 0;
-            break;
-
-
-        case 10:  // Enter key
-            choice = highlight;
-            break;
-
-        case 'q':
-            endwin();
-    	printw("Hey");
-            return 0;
-    }
+    endwin();
+    return 0;
 }
